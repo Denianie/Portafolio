5720f3b982b84663f3ac6fa0127e6ce37925e900
OntarioSAC <soporte@inmobiliariaontario.com>
2024-12-01 22:17:14 -0500

    Prueba #1 de desplieque en GitHub Pages.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  output: 'export',
+  basePath: '/portfolio', // Cambia esto al nombre de tu repositorio
+  assetPrefix: '/portfolio/', // Igual que basePath
+  images: {
+    unoptimized: true,
+  },
+
 }
 
 module.exports = nextConfig
